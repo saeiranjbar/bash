cd7d08dff39d82d557faf730d2fee050a28acdb7
Saei Ranjbar <saeiranjbar@gmail.com>
2017-09-26 11:14:51 -0700

    init commit

---
+++ cloneBash.bat
@@ -0,0 +1,4 @@
+cd ../..
+git clone https://0e1047e0e31311b62446627209c6734ab2499bc2@github.com/saeiranjbar/bash
+cd googleAutomation
+git checkout Dev
---
+++ cloneGoogleAutomation.bat
@@ -0,0 +1,4 @@
+cd ../..
+git clone https://0e1047e0e31311b62446627209c6734ab2499bc2@github.com/Kiandr/googleAutomation
+cd googleAutomation
+git checkout Dev
